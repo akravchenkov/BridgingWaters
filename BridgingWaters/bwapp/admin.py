97b2270574b7bbad7068762fc1a7e839f731f5c7
James Seppi <james.seppi@gmail.com>
2011-12-01 15:30:21 -0500

    filling out models

--- BridgingWaters/bwapp/admin.py
+++ BridgingWaters/bwapp/admin.py
@@ -9,12 +9,61 @@ class DefaultAdmin(admin.ModelAdmin):
     
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'country')
+   
+
+class OrganizationAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name', 'website']}),
+        ('Address', {'fields': ['add_street1', 'add_street2', 'add_city',
+                                'add_state_prov', 'add_code', 'add_country']}),
+        (None, {'fields': ['notes']})
+    ]
+
+class OrganizationInline(admin.StackedInline):
+    model = bwapp.models.Organization
+    fieldsets = [
+        (None, {'fields': ['name', 'website']}),
+        ('Address', {'fields': ['add_street1', 'add_street2', 'add_city',
+                                'add_state_prov', 'add_code', 'add_country']}),
+        (None, {'fields': ['notes']})
+    ]
+    extra = 0
+    
+    
+class ProjectContactInline(admin.StackedInline):
+    model = bwapp.models.ProjectContact
+    fieldsets = [
+        (None, {'fields': ['given_name', 'middle_name', 'surname', 'phone',
+                           'email', 'website']}),
+        ('Address', {'fields': ['add_street1', 'add_street2', 'add_city',
+                                'add_state_prov', 'add_code', 'add_country']}),
+        (None, {'fields': ['project']})
+    ]
+    extra = 0
+
+class HumanResContactInline(admin.StackedInline):
+    model = bwapp.models.HumanResContact
+    fieldsets = [
+        (None, {'fields': ['given_name', 'middle_name', 'surname', 'phone',
+                           'email', 'type', 'other_type']}),
+        ('Address', {'fields': ['add_street1', 'add_street2', 'add_city',
+                                'add_state_prov', 'add_code', 'add_country']}),
+        (None, {'fields': ['notes', 'project']})
+    ]
+    extra = 0
+
+class ProjectAdmin(DefaultAdmin):
+    inlines = [ProjectContactInline, HumanResContactInline]
 
 admin.site.register(bwapp.models.NewsUpdate, DefaultAdmin)
-admin.site.register(bwapp.models.Project, DefaultAdmin)
+admin.site.register(bwapp.models.Project, ProjectAdmin)
 admin.site.register(bwapp.models.FeaturedProject)
 admin.site.register(bwapp.models.Location, LocationAdmin)
 admin.site.register(bwapp.models.CodeRegion)
 admin.site.register(bwapp.models.CodeElevation)
 admin.site.register(bwapp.models.CodeTopography)
 admin.site.register(bwapp.models.CodeProjType)
+admin.site.register(bwapp.models.Organization, OrganizationAdmin)
+
+
+
--- BridgingWaters/bwapp/models.py
+++ BridgingWaters/bwapp/models.py
@@ -40,6 +40,19 @@ class CodeProjType(models.Model):
     
     def __unicode__(self):
         return self.value
+
+class CodeProfession(models.Model):
+    code = models.IntegerField(primary_key=True)
+    value = models.CharField(max_length=40)
+    
+    def __unicode__(self):
+        return self.value
+
+class Keywords(models.Model):
+    value = models.CharField(max_length=30)
+    
+    def __unicode__(self):
+        return self.value
     
 class Location(models.Model):
     country = models.CharField(max_length=30)
@@ -52,23 +65,50 @@ class Location(models.Model):
     
     def __unicode__(self):
         return "%s (%s)" % (self.name, self.country)
-        
+
+class ContactInfo(models.Model):
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    add_street1 = models.CharField(max_length=80, verbose_name="Street",
+                                   null=True, blank=True)
+    add_street2 = models.CharField(max_length=80, verbose_name="Street 2",
+                                   null=True, blank=True)
+    add_city = models.CharField(max_length=80, verbose_name="City",
+                                null=True, blank=True)
+    add_state_prov = models.CharField(max_length=30,
+                                      verbose_name="State/Province",
+                                      null=True, blank=True)
+    add_code = models.CharField(max_length=20, verbose_name="Postal Code",
+                                null=True, blank=True)
+    add_country = models.CharField(max_length=40, verbose_name="Country",
+                                   null=True, blank=True)
+    website = models.CharField(max_length=256, null=True, blank=True)
+    
+    class Meta:
+        abstract = True
+
+class Organization(ContactInfo):
+    name = models.CharField(max_length=40)
+    notes = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.org_name
+    
 class Project(models.Model):
     title = models.CharField(max_length=256)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
     goal = models.CharField(max_length=768)
-    proj_mgmt = models.TextField()
+    proj_mgmt = models.TextField(verbose_name="Project Management")
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    #keywords
+    keywords = models.ManyToManyField(Keywords, null=True, blank=True)
     #owner = User
-    proj_types = models.ManyToManyField(CodeProjType, null=True)
-    #organizations
-    #proj_contacts
-    #hum_res_contacts
+    proj_types = models.ManyToManyField(CodeProjType,
+                            verbose_name="Project Type", null=True, blank=True)
     location = models.ForeignKey(Location)
+    organization = models.ManyToManyField(Organization)
     #climate
     #comm_info
     #geo_cond
@@ -82,6 +122,29 @@ class Project(models.Model):
     def __unicode__(self):
         return self.title
    
+class HumanResContact(ContactInfo):
+    given_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30, null=True, blank=True)
+    surname = models.CharField(max_length=30)
+    type = models.ForeignKey(CodeProfession, verbose_name="Profession")
+    other_type = models.CharField(max_length='30',
+                                  verbose_name="Profession (if other)",
+                                  null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
+    project = models.ForeignKey(Project)
+    
+    def __unicode__(self):
+        return "%s %s - %s" % (self.given_name, self.surname, self.type.value)
+    
+class ProjectContact(ContactInfo): 
+    given_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30, null=True, blank=True)
+    surname = models.CharField(max_length=30)
+    project = models.ForeignKey(Project)
+    
+    def __unicode__(self):
+        return "%s %s" % (self.given_name, self.surname)
+    
 class FeaturedProject(models.Model):
     project = models.ForeignKey(Project)
     created = models.DateTimeField(auto_now_add=True)

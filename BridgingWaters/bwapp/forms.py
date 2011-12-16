62bf917c1bfbba486cdd13bca234c19ddf82cf6d
James Seppi <james.seppi@gmail.com>
2011-12-15 19:16:03 -0500

    working on add forms

--- BridgingWaters/bwapp/forms.py
+++ BridgingWaters/bwapp/forms.py
@@ -13,19 +13,24 @@ import bwapp.models as m
 now = datetime.datetime.now()
 YEARS = [str(i) for i in range(1980,now.year+1)]
 
+EMPTY_CHOICE = (False,'----')
+
 PROJ_TYPES = [(obj.code, obj.value) for obj in m.CodeProjType.objects.all()]
-ELEVATIONS = [(obj.code, obj.value) for obj in m.CodeElevation.objects.all()]
-REGIONS = [(obj.code, obj.value) for obj in m.CodeRegion.objects.all()]
-TOPOGRAPHIES = [(obj.code, obj.value) for obj in m.CodeTopography.objects.all()]
-CLIM_ZONES = [(obj.code, obj.value) for obj in m.CodeClimateZone.objects.all()]
-PRECIP_LEVELS = [(obj.code, obj.value) for obj in m.CodePrecipLevel.objects.all()]
-RESOURCES = [(obj.code, obj.value) for obj in m.CodeResource.objects.all()]
-PROFESSIONS = [(obj.code, obj.value) for obj in m.CodeProjType.objects.all()]
-URB_RURAL = [(obj.code, obj.value) for obj in m.CodeUrbanRural.objects.all()]
-PPL_SERVED = [(obj.code, obj.value) for obj in m.CodePplServed.objects.all()]
-WATER_MGMT_LEVELS = [(obj.code, obj.value) for obj in
+
+ELEVATIONS = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodeElevation.objects.all()]
+REGIONS = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodeRegion.objects.all()]
+TOPOGRAPHIES = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodeTopography.objects.all()]
+CLIM_ZONES = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodeClimateZone.objects.all()]
+PRECIP_LEVELS = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodePrecipLevel.objects.all()]
+RESOURCES = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodeResource.objects.all()]
+PROFESSIONS = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodeProjType.objects.all()] + [(99,'Other')]
+URB_RURAL = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodeUrbanRural.objects.all()]
+PPL_SERVED = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodePplServed.objects.all()]
+WATER_MGMT_LEVELS = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in
     m.CodeWaterMgmtLevel.objects.all()]
-SOIL_TYPES = [(obj.code, obj.value) for obj in m.CodeSoilType.objects.all()]
+SOIL_TYPES = [EMPTY_CHOICE] + [(obj.code, obj.value) for obj in m.CodeSoilType.objects.all()]
+
+YES_NO = [('False','No'),('True','Yes')]
 
 EXISTING_KEYWORDS = [(obj.id, obj.value) for obj in m.Keywords.objects.all()]
 
@@ -70,15 +75,12 @@ class ProjectClimateForm(forms.Form):
     climate_zone = forms.ChoiceField(widget=forms.Select, choices=CLIM_ZONES)
     precipitation = forms.ChoiceField(widget=forms.Select,
                                       choices=PRECIP_LEVELS)
-    has_rainy_season = forms.ChoiceField(widget=forms.Select,
-                            choices=(('False','No'),('True','Yes')))
+    has_rainy_season = forms.ChoiceField(widget=forms.Select, choices=YES_NO)
     rainy_months = forms.MultipleChoiceField(
         widget=forms.CheckboxSelectMultiple, choices=MONTHS, required=False)
     
 class ProjectOrgForm(forms.Form):
-    #organizations -- need to be able to add multiple
-    #-- try using formset?
-    #-- https://docs.djangoproject.com/en/1.3/topics/forms/formsets/
+    #organizations
     name = forms.CharField(max_length=40, label='Organization Name', 
         widget=forms.TextInput(attrs={'class':'title'}))
     phone = forms.CharField(max_length=20)
@@ -92,11 +94,75 @@ class ProjectOrgForm(forms.Form):
     add_country = forms.ChoiceField(widget=forms.Select, label="Country",
                                     choices=countries.COUNTRIES)
     notes = forms.CharField(widget=forms.Textarea, required=False)
+
+class ProjectCommunityForm(forms.Form):
+    urban_rural = forms.ChoiceField(widget=forms.Select, choices=URB_RURAL,
+        label="Urban/Rural")
+    description = forms.CharField(widget=forms.Textarea)
+    num_ppl_served = forms.ChoiceField(widget=forms.Select, choices=PPL_SERVED,
+        label="Number of People Served")
+    community_size = forms.IntegerField(label="Community Size")
+    water_mgmt_level = forms.ChoiceField(widget=forms.Select, choices=WATER_MGMT_LEVELS,
+        label="Water Management Level",
+        help_text="Level of government at which water is managed.")
+
+class ProjectContactsForm(forms.Form):
+    given_name = forms.CharField(max_length=30, label="Given Name")
+    middle_name = forms.CharField(max_length=30, required=False, 
+        label="Middle Name/Initial")
+    surname = forms.CharField(max_length=30)
+    phone = forms.CharField(max_length=20)
+    email = forms.EmailField()
+    add_street1 = forms.CharField(max_length=80, label="Street Address")
+    add_street2 = forms.CharField(max_length=80, label="Street Address 2",
+                                  required=False)
+    add_city = forms.CharField(max_length=80, label="City")
+    add_state_prov = forms.CharField(max_length=30, label="State/Province")
+    add_code = forms.CharField(max_length=20, label="Postal Code")
+    add_country = forms.ChoiceField(widget=forms.Select, label="Country",
+                                    choices=countries.COUNTRIES)
     
-class ProjectFormWizard(FormWizard):
-    def done(self, request, form_list):
-        save_project(form_list)
-        return HttpResponseRedirect('/bw/projects/submitted/')
+class ProjectGeoCondsForm(forms.Form):
+    soil_type = forms.ChoiceField(widget=forms.Select, choices=SOIL_TYPES,
+        label="Primary Soil Type",
+        help_text="Select the soil type most prevalent in your project area.")
+    description = forms.CharField(widget=forms.Textarea, required=False,
+        help_text="Describe the geological and soil conditions of your project area.")
+    hit_bedrock = forms.ChoiceField(widget=forms.Select, choices=YES_NO,
+        label="Hit Bedrock?",
+        help_text="Did you encounter bedrock while implementing your project?")
+    hit_water_table = forms.ChoiceField(widget=forms.Select, choices=YES_NO,
+        label="Hit Water Table?",
+        help_text="Did you encounter the water table while implementing your project?")
+    impact = forms.CharField(widget=forms.Textarea, required=False,
+        label="Impact of Geological Conditions",
+        help_text="Describe how the geological and soil conditions impacted your project.")
         
-def save_project(form_list):
-    pass
+class ProjectHumanResForm(forms.Form):
+    given_name = forms.CharField(max_length=30, label="Given Name")
+    middle_name = forms.CharField(max_length=30, required=False, 
+        label="Middle Name/Initial")
+    surname = forms.CharField(max_length=30)
+    profession = forms.ChoiceField(widget=forms.Select, choices=PROFESSIONS)
+    
+    #TODO: Hide the label and help_text also
+    profession_other = forms.CharField(max_length=30, required=False, 
+        label="Profession (other)",
+        help_text="Please input a profession if you selected 'Other'",
+        widget=forms.TextInput(attrs={'class':''})) #TODO: class hide
+    
+    phone = forms.CharField(max_length=20, required=False)
+    email = forms.EmailField(required=False)
+    add_street1 = forms.CharField(max_length=80, label="Street Address",
+                                   required=False)
+    add_street2 = forms.CharField(max_length=80, label="Street Address 2",
+                                  required=False)
+    add_city = forms.CharField(max_length=80, label="City", required=False)
+    add_state_prov = forms.CharField(max_length=30, label="State/Province",
+                                   required=False)
+    add_code = forms.CharField(max_length=20, label="Postal Code", required=False)
+    add_country = forms.ChoiceField(widget=forms.Select, label="Country",
+                                    choices=countries.COUNTRIES,
+                                    required=False)
+    notes = forms.CharField(widget=forms.Textarea, required=False)
+    
--- BridgingWaters/bwapp/views.py
+++ BridgingWaters/bwapp/views.py
@@ -6,7 +6,10 @@ from django.shortcuts import (render, get_object_or_404,
 from django.forms.formsets import formset_factory
 from random import choice
 
+STEP_COUNT = 8
 
+#TODO: If use the save button, save current responses to database, redirect to same form
+#TODO: If use the save and continute, save current responses to database, redirect to next form
 
 def index(request):
     latest_news_list = \
@@ -48,7 +51,7 @@ def project_add_step1(request):
     return render(request, 'forms/project_add_basic.html', {
         'step_title':'Basic Information',
         'step':1,
-        'step_count':5,
+        'step_count':STEP_COUNT,
         'form':form
         })
 
@@ -66,7 +69,7 @@ def project_add_step2(request):
     return render(request, 'forms/project_add_basic.html', {
         'step_title':'Project Location',
         'step':2,
-        'step_count':5,
+        'step_count':STEP_COUNT,
         'form':form
         })
 
@@ -84,12 +87,12 @@ def project_add_step3(request):
     return render(request, 'forms/project_add_basic.html', {
         'step_title':'Climate',
         'step':3,
-        'step_count':5,
+        'step_count':STEP_COUNT,
         'form':form
         })
 
 def project_add_step4(request):
-    
+    #organizations
     ProjectOrgFormSet = formset_factory(f.ProjectOrgForm, max_num=5)
     
     if request.method == "POST":
@@ -101,16 +104,96 @@ def project_add_step4(request):
     else:
         formset = ProjectOrgFormSet()
 
-    return render(request, 'forms/project_add_orgs.html', {
+    return render(request, 'forms/project_add_formset.html', {
         'step_title':'Involved Organizations',
         'step':4,
-        'step_count':5,
-        'formset':formset
+        'step_count':STEP_COUNT,
+        'formset':formset,
+        'legend':"Involved Organization Information"
         })
 
 def project_add_step5(request):
-    pass
+    #community
+    if request.method == "POST":
+        #Process, save in session, and redirect to next
+        form = f.ProjectCommunityForm(request.POST)
+        
+        if form.is_valid():
+            request.session['community_form'] = form
+            return redirect(project_add_step6)
+    else:
+        form = f.ProjectCommunityForm()
+        
+    return render(request, 'forms/project_add_basic.html', {
+        'step_title':'Community Information',
+        'step':5,
+        'step_count':STEP_COUNT,
+        'form':form
+        })
+    
+def project_add_step6(request):
+    #geological conditions
+    if request.method == "POST":
+        #Process, save in session, and redirect to next
+        form = f.ProjectGeoCondsForm(request.POST)
+        
+        if form.is_valid():
+            request.session['geo_form'] = form
+            return redirect(project_add_step7)
+    else:
+        form = f.ProjectGeoCondsForm()
+        
+    return render(request, 'forms/project_add_basic.html', {
+        'step_title':'Geological Conditions',
+        'step':6,
+        'step_count':STEP_COUNT,
+        'form':form
+        })
 
+def project_add_step7(request):
+    #contacts
+    ProjectContactFormSet = formset_factory(f.ProjectContactsForm, max_num=5)
+    
+    if request.method == "POST":
+        #Process, save in session, and redirect to next
+        formset = ProjectContactFormSet(request.POST)
+        if formset.is_valid():
+            request.session['contacts_formset'] = formset
+            return redirect(project_add_step8)
+    else:
+        formset = ProjectContactFormSet()
+
+    return render(request, 'forms/project_add_formset.html', {
+        'step_title':'Project Contacts',
+        'step':7,
+        'step_count':STEP_COUNT,
+        'formset':formset,
+        'legend':"Contact Information"
+        })
+
+def project_add_step8(request):
+    #human resource contacts
+    ProjectHumResFormSet = formset_factory(f.ProjectHumanResForm, max_num=5)
+    
+    if request.method == "POST":
+        #Process, save in session, and redirect to next
+        formset = ProjectHumResFormSet(request.POST)
+        if formset.is_valid():
+            request.session['humres_formset'] = formset
+            return redirect(project_add_step8)
+    else:
+        formset = ProjectHumResFormSet()
+
+    return render(request, 'forms/project_add_formset.html', {
+        'step_title':'Human Resources',
+        'step':8,
+        'step_count':STEP_COUNT,
+        'formset':formset,
+        'legend':"Human Resources Contact Information"
+        })
+    
+    
+    
 def project_submitted(request):
     #TODO: get recently submitted project information to display on the page
     return render(request, 'bwapp/project_submitted.html')
